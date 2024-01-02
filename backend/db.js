93d66cf9582fd83af5532bd372272b79e4dc8aa8
gupta <saurabh4kaj@gmail.com>
2024-01-02 16:19:21 +0530

    connection to database is done

---
+++ backend/db.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+const connection = mongoose.connect('mongodb+srv://saurabh:saurabh@cluster0.hovcp.mongodb.net/Charitism_todo?retryWrites=true&w=majority')
+module.exports={
+	connection
+}
